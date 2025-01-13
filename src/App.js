2525e5fdf3c0ef0ce864c04a0ba24623ef602b19
DiktatorMira <112933359+DiktatorMira@users.noreply.github.com>
2025-01-13 23:54:14 +0200

    Большие изменения на странице задач

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import { Tasks } from './Components/Tasks';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Auth from './Components/Auth';
-import Main from './Components/Main';
 
 export function App() {
   return (
@@ -12,8 +10,7 @@ export function App() {
       <Routes>
         <Route path="/tasks" element={<Tasks />} />
         <Route path="/" element={<Auth />} />
-        <Route path="/main" element={<Main />} />
       </Routes>
     </Router>
   );
-};
+}
